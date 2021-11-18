2018ecfa27f0d0bbd8b38898ef7f8915f4e6fc72
Jonathan Moyes <jon@moyes.com>
2021-11-17 21:31:52 -0700

    blind progress, need to test on-device

--- util/gate_hardware_test.py
+++ util/gate_hardware_test.py
@@ -1,37 +1,30 @@
-"""
-This is the stargate program for running the stargate from https://thestargateproject.com
-This main.py file is run automatically on boot. It is executed in the .bashrc file for the sg1 user.
-"""
-from helper_functions import log, software_update, check_internet_connection, set_correct_audio_output_device
+#!/usr/bin/python3
+import sys
+import os
+sys.path.append('classes')
+sys.path.append('config')
+
+from stargate_config import StargateConfig
+from ancients_log_book import AncientsLogBook
+from software_update import SoftwareUpdate
+from stargate_audio import StargateAudio
+
 from time import sleep
-from classes.HardwareDetector import HardwareDetector
+from HardwareDetector import HardwareDetector
 from chevrons import chevrons
-    
+
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper as stp
 import neopixel, board
-        
-### Check/set the correct USB audio adapter. This is necessary because different raspberries detects the USB audio adapter differently.
-try:
-    set_correct_audio_output_device()
-except:
-    pass
 
-### Boot up the Stargate ###
-#from classes.STARGATE_SG1 import StargateSG1
+### Load our config file
+cfg = StargateConfig()
 
-#Create the Stargate object
-log('sg1.log', f'Test Starting')
+### Initialize the audio stuffs
+audio = StargateAudio(log)
+audio.set_correct_audio_output_device()
+
 print (f'Test Starting')
-# stargate = StargateSG1()
-
-
-
-total_steps = 1250
-micro_steps = 16
-stepper_pos = 0
-stepper = None
-
 
 # ########### WORMHOLE NEOPIXEL TESTS ###############
 
@@ -59,8 +52,6 @@ while(True):
             pixels.fill((0,0,0))
             pixels.show()
         break
-    
-
 
 # #### Motor Hardware Detection #################
 
@@ -69,17 +60,20 @@ motorHardwareMode = hwDetector.getMotorHardwareMode()
 modeName = hwDetector.getMotorHardwareModeName()
 if (not modeName):
     print("Warning: No supported motor hardware controllers detected")
-    
+
 else:
     print("Motor Hardware FOUND: {}".format(modeName))
 
 # ########### SYMBOL RING MOTOR TESTS ###############
-# 
+#
+# total_steps = 1250
+# stepper = None
+
 # if motorHardwareMode > 0:
 #     stepper = MotorKit().stepper1
 #     sleep(0.4)
 #     stepper.release()
-#     
+#
 # # move a short distance clockwise
 # direct = stp.FORWARD
 # speed = 0.01
@@ -96,10 +90,10 @@ else:
 #             break
 #         else:
 #             continue
-#         
+#
 #     except KeyboardInterrupt:
 #         break
-# 
+#
 # direct = stp.BACKWARD
 # input("Moving the ring counter-clockwise. Press enter to start.")
 # while(True):
@@ -113,21 +107,21 @@ else:
 #             break
 #         else:
 #             continue
-#         
+#
 #     except KeyboardInterrupt:
 #         break
-# 
+#
 # sleep(0.4)
-# stepper.release()   
-# 
+# stepper.release()
+#
 # while(True):
 #     direct = stp.FORWARD
 #     input("CALIBRATION: The ring is unlocked. Gently move the \"Home\" symbol into the top chevron. Press enter when done.")
-# 
+#
 #     # Save the current position in the file
-# 
+#
 #     try:
-#         for i in range(1,1250):
+#         for i in range(1,total_steps):
 #             stepper.onestep(direction=direct, style=stp.DOUBLE)
 #             sleep(speed)
 #         stepper.release()
@@ -136,17 +130,17 @@ else:
 #             break
 #         else:
 #             continue
-#         
+#
 #     except KeyboardInterrupt:
 #         break
-# 
+#
 # while(True):
 #     direct = stp.BACKWARD
-# 
+#
 #     # Save the current position in the file
-# 
+#
 #     try:
-#         for i in range(1,1250):
+#         for i in range(1,total_steps):
 #             stepper.onestep(direction=direct, style=stp.DOUBLE)
 #             sleep(speed)
 #         stepper.release()
@@ -155,19 +149,21 @@ else:
 #             break
 #         else:
 #             continue
-#         
+#
 #     except KeyboardInterrupt:
 #         break
-#         
+#
 # sleep(0.4)
-# stepper.release()   
+# stepper.release()
 
 # ########### CHEVRON TESTS ###############
 
+# Load the chevron configuration and initialize their objects
+chevrons = ChevronManager()
 print( "Testing Chevron lights and motors. Motor should move inward, the light should turn on, then the motor should move outward. ")
 for i in range(1,8):
     print("Testing Chevron {}.".format(i))
-    chevrons[i].on()
+    chevrons.get(i).on()
     sleep(1)
 
 # ##########################################
