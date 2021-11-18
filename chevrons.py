343b18ae4b24d0788944d13e8dd9a6de845ab2e7
Jonathan Moyes <jon@moyes.com>
2021-11-17 18:35:39 -0700

    Add chevrons.py.dist, ignore chevrons.py

--- chevrons.py
+++ chevrons.py
@@ -6,22 +6,12 @@ from classes.HardwareDetector import HardwareDetector
 hwDetector = HardwareDetector()
 mode = hwDetector.getMotorHardwareMode()
 
-chevrons = {1: Chevron(6, 7, mode),
-            2: Chevron(13, 8, mode),
-            3: Chevron(19, 11, mode),
-            4: Chevron(21, 3, mode),
-            5: Chevron(16, 4, mode),
-            6: Chevron(20, 5, mode),
-            7: Chevron(26, 6, mode),
-            8: Chevron(None, 10, mode),
-            9: Chevron(None, 9, mode)}
-
-#chevrons = {1: Chevron(21, 3),
-#            2: Chevron(16, 4),
-#            3: Chevron(20, 5),
-#            4: Chevron(26, 6),
-#            5: Chevron(6, 7),
-#            6: Chevron(13, 8),
-#            7: Chevron(19, 9),
-#            8: Chevron(None, 10),
-#            9: Chevron(None, 11)}
+chevrons = {1: Chevron(21, 3),
+            2: Chevron(16, 4),
+            3: Chevron(20, 5),
+            4: Chevron(26, 6),
+            5: Chevron(6, 7),
+            6: Chevron(13, 8),
+            7: Chevron(19, 9),
+            8: Chevron(None, 10),
+            9: Chevron(None, 11)}
