1518cebf6e0346cb8e6ce12d393e0574a5d6c806
Jonathan Moyes <jon@moyes.com>
2021-11-21 14:21:06 -0700

    neopixel HW sim: add __setitem__()

--- classes/hardware_simulation.py
+++ classes/hardware_simulation.py
@@ -3,40 +3,40 @@ from time import sleep
 class StepperSim:
 
     def __init__(self):
-    
+
         self.onestepTime = 0.0038 # in seconds, how long does a step take to exec on real HW
         pass
-        
+
     def onestep(self, direction, style):
         sleep(self.onestepTime)
         pass
-    
+
     def release(self):
         pass
-        
+
 class DCMotorSim:
 
     def __init__(self):
         self.throttle = 0
         pass
-        
+
     def onestep(self, direction, style):
         pass
-    
+
     def release(self):
         pass
-        
-    
+
+
 class LEDSim:
     def __init__(self):
         pass
-    
+
     def on(self):
         pass
-        
+
     def off(self):
-        pass		
-		
+        pass
+
 class NeopixelSim:
 
 	def __init__(self):
@@ -48,5 +48,5 @@ class NeopixelSim:
 	def fill(self, color_tuple):
 		pass
 
-	def _getitem(self, index):
+	def __setitem__(self, index, val):
 		return None
