d11d174ccfec731040601d42ce7fc3025deb326f
Jonathan Moyes <jon@moyes.com>
2022-01-14 21:07:59 -0700

    macos compatibility

--- util/get_subspace_public_key.sh
+++ util/get_subspace_public_key.sh
@@ -4,12 +4,17 @@
 # If it does, we print the existing key
 # If not, we generate a new public key, and print it
 
-FILE=/etc/wireguard/publickey
-
-if sudo test -f "$FILE"; then
-    sudo su -c "cat /etc/wireguard/publickey"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  WGROOT=/usr/local/etc/wireguard/ # For MacOS
 else
-    sudo su -c "cd /etc/wireguard/; umask 077; wg genkey | tee privatekey | wg pubkey > publickey; cat publickey"
+  WGROOT=/etc/wireguard/publickey # For Raspi
+fi
+
+if sudo test -f "$WGROOT"/publickey; then
+    sudo cat $WGROOT/publickey
+else
+  sudo su root -c "cd "$WGROOT"; umask 077; wg genkey | tee privatekey | wg pubkey > publickey; cat publickey"
+
 fi
 
 exit 0
