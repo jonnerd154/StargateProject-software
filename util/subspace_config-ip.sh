2354199252a4652496dc8e9bd7053a63c929e63a
Jonathan Moyes <jon@moyes.com>
2022-01-15 12:59:11 -0700

    Beginnings of MacOS compat - script is disabled for Mac.

--- util/subspace_config-ip.sh
+++ util/subspace_config-ip.sh
@@ -4,19 +4,29 @@
 # Configures the static IP from vargs
 # Saves the configuration
 # Enables the subspace interface to run in default Unix running modes
-# Reboots the machine
+# Reboots the machine - currently commented out!
 
 echo "Configuring Subspace Network"
 
-if [ -z "$1" ] 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  WGROOT=/usr/local/etc/wireguard/ # For MacOS
+  exit 0 # This script doesn't currently work for MacOS - just return w/o action for now.
+else
+  WGROOT=/etc/wireguard/ # For Raspi
+fi
+
+if [ -z "$1" ]
 then
  echo "No IP Address provided"
  exit 1
 fi
 
-sudo su -c "cd /etc/wireguard/; wg-quick up subspace; wg set subspace private-key privatekey;"
-sudo su -c "ip addr add $1 dev subspace wg-quick save subspace"
-sudo su -c "systemctl enable wg-quick@subspace"
-sudo reboot
-echo "Subspace configured"
+sudo su root -c "cd "$WGROOT"; wg-quick up subspace; wg set subspace private-key privatekey;"
+sudo su root -c "ip addr add $1 dev subspace wg-quick save subspace"
+sudo su root -c "systemctl enable wg-quick@subspace"
+
+# TODO: This process needs to change on MacOS - the executables are different.
+echo "Configuration saved. Please reboot the Raspi (sudo reboot)."
+#sudo reboot
+
 exit 0
