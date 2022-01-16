8efa25ee0c6893fb678afc82a861a977885ce8b2
Jonathan Moyes <jon@moyes.com>
2022-01-15 20:33:49 -0700

    Bugfix: bad syntax

--- util/subspace_mgmt.sh
+++ util/subspace_mgmt.sh
@@ -6,10 +6,11 @@ if [ -z "$1" ]
 then
  echo "USAGE: subspace_mgmt.sh [ up | down ]"
  exit 1
-elif [[ $1 eq "up" ]]; then
+elif [[ $1 == "up" ]]; then
   sudo wg-quick up subspace
-elif [[ $1 eq "down" ]]; then
+elif [[ $1 == "down" ]]; then
   sudo wg-quick down subspace
 else
   echo "USAGE: subspace_mgmt.sh [ up | down ]"
   exit 1
+fi
