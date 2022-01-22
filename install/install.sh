aa869a1ca46dd521d2c88db1acd7afb4a4e38c4d
Jonathan Moyes <jon@moyes.com>
2022-01-22 12:00:29 -0700

    reorganize

--- install/install.sh
+++ install/install.sh
@@ -7,8 +7,11 @@ echo "TheStargateProject: SG1 Version 4  -  Installer Script - Step 2"
 echo
 echo
 
-verify_stargate_software()
-echo 'V4 Software installation detected'
+source functions.sh
+
+verify_stargate_software
+echo 'Version 4.x Software installation detected'
+
 
 echo
 echo
