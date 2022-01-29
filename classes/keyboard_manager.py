108789972c6f3343cecaa0c328308f9c035e6b9d
Jonathan Moyes <jon@moyes.com>
2022-01-29 13:18:17 -0700

    revert keyboard changes

--- classes/keyboard_manager.py
+++ classes/keyboard_manager.py
@@ -13,36 +13,11 @@ class KeyboardManager:
         self.addr_manager = stargate.addr_manager
         self.address_book = stargate.addr_manager.get_book()
 
-        self.shift_pressed = False
-
-        if sys.platform != "darwin":
-            import keyboard # pylint: disable=import-outside-toplevel
-            keyboard.on_press(self.handle_keyboard_keypress)
-            keyboard.on_release_key("shift", self.handle_keyboard_release_shift)
-
-        self.log.log("Listening for input from the DHD. You can abort with the '-' key.")
-
     @staticmethod
-    def get_key_map():
-      ## the dictionary containing the key to symbol-number relations.
-        return {'8': 1, 'C': 2, 'V': 3, 'U': 4, 'a': 5, '3': 6, '5': 7, 'S': 8, 'b': 9, 'K': 10, 'X': 11, 'Z': 12,
-                          'E': 14, 'P': 15, 'M': 16, 'D': 17, 'F': 18, '7': 19, 'c': 20, 'W': 21, '6': 22, 'G': 23, '4': 24,
-                          'B': 25, 'H': 26, 'R': 27, 'L': 28, '2': 29, 'N': 30, 'Q': 31, '9': 32, 'J': 33, '0': 34, 'O': 35,
-                          'T': 36, 'Y': 37, '1': 38, 'I': 39
-                          }
-
-    @staticmethod
-    def get_abort_characters():
-        # If these symbols are entered, the gate will shutdown
-        return [ '-', '\x03' ]  # '\x03' == Ctrl-C
-
-# ++++ START: STDIN keyboard handling (for Console/SSH) +++++++
-
-    @staticmethod
-    def wait_for_keypress_stdin():
+    def key_press():
         """
-        This helper function blocks the thread, waiting for a single character+\n from STDIN
-        :return: The pressed char is returned.
+        This helper function stops the program (thread) and waits for a single keypress.
+        :return: The pressed key is returned.
         """
 
         file_desc = sys.stdin.fileno()
@@ -52,10 +27,10 @@ class KeyboardManager:
             char = sys.stdin.read(1)
         finally:
             termios.tcsetattr(file_desc, termios.TCSADRAIN, old_settings)
+        return char
 
-        self.handle_keypress_char(char)
 
-    def service_stdin(self):
+    def ask_for_input(self):
         """
         This function takes the stargate as input and listens for user input (from the DHD or keyboard). The pressed key
         is converted to a stargate symbol number as seen in this document: https://www.rdanderson.com/stargate/glyphs/index.htm
@@ -63,62 +38,48 @@ class KeyboardManager:
         :return: Nothing is returned, but the stargate is manipulated.
         """
 
+        ## the dictionary containing the key to symbol-number relations.
+        key_symbol_map = {'8': 1, 'C': 2, 'V': 3, 'U': 4, 'a': 5, '3': 6, '5': 7, 'S': 8, 'b': 9, 'K': 10, 'X': 11, 'Z': 12,
+                          'E': 14, 'P': 15, 'M': 16, 'D': 17, 'F': 18, '7': 19, 'c': 20, 'W': 21, '6': 22, 'G': 23, '4': 24,
+                          'B': 25, 'H': 26, 'R': 27, 'L': 28, '2': 29, 'N': 30, 'Q': 31, '9': 32, 'J': 33, '0': 34, 'O': 35,
+                          'T': 36, 'Y': 37, '1': 38, 'I': 39
+                          }
+
+        # If these symbols are entered, the gate will shutdown
+        abort_characters = [ '-', '\x03' ]  # '\x03' == Ctrl-C
+
         self.log.log("Listening for input from the Dialer. You can abort with the '-' key.")
-        while self.stargate.running: # Keep running and ask for user input
-            char = self.wait_for_keypress_stdin() #Save the input key as a variable
-            self.handle_keypress_char(char)
+        while True: # Keep running and ask for user input
+            key = self.key_press() #Save the input key as a variable
+            ## Convert the key to the correct symbol_number. ##
+            try:
+                symbol_number = key_symbol_map[key]  # convert key press to symbol_number
+            except KeyError:  # if the pressed button is not a key in the self.key_symbol_map dictionary
+                symbol_number = 'unknown'
+                if key in abort_characters:
+                    symbol_number = 'abort'
+                elif key == 'A':
+                    symbol_number = 'centre_button_outgoing'
+                    self.log.log(f'key: {key} -> symbol: {symbol_number} CENTER')
+                else:
+                    self.log.log(f'key: {key} -> symbol: {symbol_number} SYMBOL')
 
-# ++++ END: STDIN keyboard handling +++++++
+            ## If the user inputs the - key to abort. Not possible from the DHD.
+            if key in abort_characters:
+                self.log.log("Abort Requested: Shutting down any active wormholes, stopping the gate.")
+                self.stargate.wormhole_active = False # Shutdown any open wormholes (particularly if turned on via web interface)
+                self.stargate.running = False  # Stop the stargate object from running.
 
-# ++++ START: raw keyboard handling (for DHD)+++++++
+                break # This will break us out of the while loop and end the function.
 
-    def handle_keyboard_keypress(self, key):
-        if key.name == 'shift':
-            self.shift_pressed = True
-        else:
-            if self.shift_pressed:
-                key.name = key.name.upper()
+            ## If the user hits the centre_button
+            if key == 'A':
+                self.queue_center_button()
 
-        self.handle_keypress_char(key.name)
-
-    def handle_keyboard_release_shift(self, event): # pylint: disable=unused-argument
-        self.shift_pressed = False
-
-# ++++ END: raw keyboard handling (for DHD)+++++++
-
-    def handle_keypress_char(self, char):
-        # This function takes a character read in by either input method, looks
-        #  up the associated symbol index (if valid), and queues the symbol
-        #  to the outgoing_symbol_buffer
-
-        ## Convert the key to the correct symbol_number. ##
-        try:
-            symbol_number = self.get_key_map()[char]  # convert key press to symbol_number
-        except KeyError:  # if the pressed button is not a key in the self.key_symbol_map dictionary
-            symbol_number = 'unknown'
-            if char in self.get_abort_characters():
-                symbol_number = 'abort'
-            elif char == 'A':
-                symbol_number = 'centre_button_outgoing'
-                self.log.log(f'key: {char} -> symbol: {symbol_number} CENTER')
+            # If we are hitting symbols on the DHD.
             else:
-                self.log.log(f'key: {char} -> symbol: {symbol_number} SYMBOL')
+                self.queue_symbol(symbol_number)
 
-        ## If the user inputs the - key to abort. Not possible from the DHD.
-        if char in self.get_abort_characters():
-            self.log.log("Abort Requested: Shutting down any active wormholes, stopping the gate.")
-            self.stargate.wormhole_active = False # Shutdown any open wormholes (particularly if turned on via web interface)
-            self.stargate.running = False  # Stop the stargate object from running.
-
-        ## If the user hits the centre_button
-        if char == 'A':
-            self.queue_center_button()
-
-        # If we are hitting symbols on the DHD.
-        else:
-            self.queue_symbol(symbol_number)
-
-    # Move to Dialer
     def queue_symbol(self, symbol_number):
         self.audio.play_random_clip("DHD")
         if symbol_number != 'unknown' and symbol_number not in self.stargate.address_buffer_outgoing:
@@ -130,7 +91,6 @@ class KeyboardManager:
                 self.stargate.address_buffer_outgoing.append(symbol_number)
                 self.log.log(f'address_buffer_outgoing: {self.stargate.address_buffer_outgoing}') # Log the address_buffer
 
-    # Move to Dialer
     def queue_center_button(self):
         self.audio.play_random_clip("DHD")
         # If we are dialing
