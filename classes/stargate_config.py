91bf4a14e2424b03c7f49447200c7d2fdac6f748
Jonathan Moyes <jon@moyes.com>
2022-02-04 16:04:33 -0700

    Remove passed-defaults capability

--- classes/stargate_config.py
+++ classes/stargate_config.py
@@ -10,11 +10,10 @@ sys.path.append('config')
 
 class StargateConfig:
 
-    def __init__(self, base_path, file_name, defaults=None):
+    def __init__(self, base_path, file_name):
 
         self.file_name = file_name
         self.conf_dir = base_path + "/config" #No trailing slash
-        self.defaults = defaults
         self.log = None # call set_log when log is available
 
         self.config = None
@@ -28,14 +27,10 @@ class StargateConfig:
             with open(self.get_full_file_path(), "r", encoding="utf8") as file:
                 self.config = json.load(file)
         except FileNotFoundError:
-            # If the file wasn't found, and we were given defaults, initialize the file.
-            if self.defaults is not None:
-                print(f"*** Initializing configuration with provided defaults {self.file_name}")
-                self.set_defaults()
-            else:
-                # The Config file doesn't exist, and were weren't passed defaults.
-                # Check if there's a default config available to load
-                self.copy_default_config_file()
+            # The Config file doesn't exist
+            # Check if there's a default config available to load
+            self.copy_default_config_file()
+            self.log.log(f"*** Configuration initialized with default file {self.file_name}")
 
 
     def copy_default_config_file(self):
@@ -49,18 +44,11 @@ class StargateConfig:
             sys.exit(1)
             return
 
-        print(f"Loaded default configuration file for {self.file_name}.")
-
         # Call load() so we can use the newly loaded config
         self.load()
 
     def set_log(self, log):
         self.log = log
-
-    def set_defaults(self):
-        if self.defaults is not None:
-            self.config = self.defaults
-            self.save()
 
     def get_full_file_path(self):
         return self.conf_dir+"/"+self.file_name
