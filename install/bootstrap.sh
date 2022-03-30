2a6f174312ab4d4d67773e7f40e7d8837c017999
Jonathan Moyes <jon@moyes.com>
2022-03-29 21:04:19 -0600

    tweak bootstrap script

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get install git
 cd ~
-git clone https://github.com/jonnerd154/StargateProject2021.git sg1_v4
+git clone https://github.com/jonnerd154/StargateProject-software.git sg1_v4
+sudo chmod u+x /home/pi/sg1_v4/install/*.sh
+cd /home/pi/sg1_v4/install && ./install.sh
