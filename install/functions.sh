a2e3cdebca49e2b489178c876b315d3be9f52c78
Jonathan Moyes <jon@moyes.com>
2022-04-01 08:58:08 -0600

    fix crontab edit when tab doesn't exist
    addresses #92

--- install/functions.sh
+++ install/functions.sh
@@ -144,7 +144,7 @@ function restart_apache() {
 function configure_crontab() {
   # Add the speaker-tickler to our crontab
   echo 'Configuring crontab (user: pi)'
-  (crontab -l; echo '*/8 * * * * /home/pi/venv_v4/bin/python3 /home/pi/sg1_v4/scripts/speaker_on.py')|awk '!x[$0]++'|crontab -
+  (crontab -l 2>/dev/null; echo "*/8 * * * * /home/pi/venv_v4/bin/python3 /home/pi/sg1_v4/scripts/speaker_on.py") | awk '!x[$0]++' | crontab -
 }
 
 function disable_pwr_mgmt() {
