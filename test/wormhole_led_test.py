9e8bf177f67e41988d79b6a6e93d0b09350c4d56
Jonathan Moyes <jon@moyes.com>
2022-05-04 18:45:13 +0100

    allow selection of color order

--- test/wormhole_led_test.py
+++ test/wormhole_led_test.py
@@ -7,7 +7,7 @@ from time import sleep
 
 Test Script for Wormhole Neopixel LEDs
 
-This script tests a 122-pixel long chain of LEDS, cycling between 
+This script tests a 122-pixel long chain of LEDS, cycling between
 red, green, and blue colors at a moderate brightness
 
 TheStargateProject.com / BuildAStargate.com
@@ -23,26 +23,30 @@ delay = 0.5     # In seconds, time to show each color
 
 # Color tuples are ( RED, GREEN, BLUE )
 # Define a list to cycle through
-colors = [ 
+colors = [
   (brightness, 0, 0), # Red
   (0, brightness, 0), # Green
-  (0, 0, brightness)  # Blue
+  (0, 0, brightness), # Blue
+  (0, 0, 0)           # Off
 ]
+
+pixel_order = neopixel.GRB    # Default 
+#pixel_order = neopixel.GRBW  # Some SK6812
 
 # --------------------------------------------
 
 # Initialize the strip
-pixels = neopixel.NeoPixel(board.D12, 122)
+pixels = neopixel.NeoPixel(board.D12, 122, pixel_order=pixel_order)
 
 # Test until keyboard interrupt
 try:
   print("Testing Neopixel strip. Press ctrl+c to stop.")
-  while 1:  
+  while 1:
     for color_tuple in colors:
       pixels.fill(color_tuple)
       pixels.show()
       sleep(0.5)
-  
+
 except KeyboardInterrupt:
   pixels.fill((0,0,0))
   pixels.show()
