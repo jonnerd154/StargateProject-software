db39746be1c18f39d26ca8e49eaf89c344463566
Jonathan Moyes <jon@moyes.com>
2022-05-04 03:35:25 +0100

    add wh neo test script

---
+++ test/wormhole_led_test.py
@@ -0,0 +1,49 @@
+import sys
+import board
+import neopixel
+from time import sleep
+
+'''
+
+Test Script for Wormhole Neopixel LEDs
+
+This script tests a 122-pixel long chain of LEDS, cycling between 
+red, green, and blue colors at a moderate brightness
+
+TheStargateProject.com / BuildAStargate.com
+
+'''
+
+# --------------------------------------------
+
+# CONFIGURATION
+
+brightness = 10 # 0-255. Recommend not exceeding 127.
+delay = 0.5     # In seconds, time to show each color
+
+# Color tuples are ( RED, GREEN, BLUE )
+# Define a list to cycle through
+colors = [ 
+  (brightness, 0, 0), # Red
+  (0, brightness, 0), # Green
+  (0, 0, brightness)  # Blue
+]
+
+# --------------------------------------------
+
+# Initialize the strip
+pixels = neopixel.NeoPixel(board.D12, 122)
+
+# Test until keyboard interrupt
+try:
+  print("Testing Neopixel strip. Press ctrl+c to stop.")
+  while 1:  
+    for color_tuple in colors:
+      pixels.fill(color_tuple)
+      pixels.show()
+      sleep(0.5)
+  
+except KeyboardInterrupt:
+  pixels.fill((0,0,0))
+  pixels.show()
+  sys.exit(0)
